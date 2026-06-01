fba012eebf1ba22d0bc1f4df2c97f189339558c1
Lautaro-Barreto <barretolautaroagustin36@gmail.com>
2026-06-01 17:10:53 -0300

    Fix database creation script

--- Scripts/Creacion de la BBDD/create-database.sql
+++ Scripts/Creacion de la BBDD/create-database.sql
@@ -5,14 +5,18 @@ USE master
 GO
 
 -- Drop the database if it already exists
-IF  EXISTS (
-	SELECT name 
-		FROM sys.databases 
-		WHERE name = N'[SGParquesNacionales]'
-)
-DROP DATABASE [SGParquesNacionales]
-GO
-
-CREATE DATABASE [SGParquesNacionales]
+IF  EXISTS (SELECT name FROM sys.databases WHERE name = 'SGParquesNacionales')
+BEGIN
+	-- Forzar modo SINGLE_USER y cerrar todas las conexiones
+	print('altering database to single user');
+	ALTER DATABASE SGParquesNacionales
+	SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	-- Eliminar la base
+	print('deleting database');
+	DROP DATABASE SGParquesNacionales;
+	print('deleted database');
+	CREATE DATABASE SGParquesNacionales;
+	print('created database SGParquesNacionales')
+END
 GO
 
