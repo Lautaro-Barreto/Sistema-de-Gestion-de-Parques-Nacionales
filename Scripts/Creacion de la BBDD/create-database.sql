a9984dd6ad760306fa3c616168ccea087fcbc00a
KiyeWolf <guillemirandadev@gmail.com>
2026-06-25 19:34:38 -0300

    Merge
    -Hice merge de la entrega 8.
    - :)

--- Scripts/Creacion de la BBDD/create-database.sql
+++ Scripts/Creacion de la BBDD/create-database.sql
@@ -1,7 +1,3 @@
--- =============================================
---			CREACION DE LA BASE DE DATOS
--- =============================================
-
 /*
 #Universidad Nacional de la Matanza
 #Materia: 3641 - Bases de Datos Aplicada 
@@ -9,8 +5,27 @@
 #Integrantes: Barreto Lautaro, Losada Agustina, Miranda Guillermo, Villar Facundo
 #Descripción: En este sript se crea la base de datos "SGParquesNacionales"
 */
-USE master
-go
+-- =============================================
+-- _Creación de la Base de datos.
+-- =============================================
+
+/*
+-- Como borrar la BBDD
+
+-- Cambiar al contexto master
+USE master;
+GO
+
+-- Forzar modo SINGLE_USER y cerrar todas las conexiones
+ALTER DATABASE SGParquesNacionales
+SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+GO
+
+-- Eliminar la base
+DROP DATABASE SGParquesNacionales;
+GO
+*/
+
 
 IF NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = 'SGParquesNacionales')
 BEGIN
